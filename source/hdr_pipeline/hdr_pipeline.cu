c6424ae23d8f0f0ecbf20d6fd0c8d9caa3058054
elamre <e.vanrijnswou@innoseis.com>
2017-02-16 18:20:34 +0100

    matthijs

--- source/hdr_pipeline/hdr_pipeline.cu
+++ source/hdr_pipeline/hdr_pipeline.cu
@@ -21,97 +21,148 @@ namespace
 		math::float2(-12.00f, 0.01107369f),
 		math::float2(-11.00f, 0.01459965f),
 		math::float2(-10.00f, 0.01879116f),
-		math::float2( -9.00f, 0.02361161f),
-		math::float2( -8.00f, 0.02896398f),
-		math::float2( -7.00f, 0.03468581f),
-		math::float2( -6.00f, 0.04055144f),
-		math::float2( -5.00f, 0.04628301f),
-		math::float2( -4.00f, 0.05157007f),
-		math::float2( -3.00f, 0.05609637f),
-		math::float2( -2.00f, 0.05957069f),
-		math::float2( -1.00f, 0.06175773f),
-		math::float2(  0.00f, 0.06250444f),
-		math::float2(  1.00f, 0.06175773f),
-		math::float2(  2.00f, 0.05957069f),
-		math::float2(  3.00f, 0.05609637f),
-		math::float2(  4.00f, 0.05157007f),
-		math::float2(  5.00f, 0.04628301f),
-		math::float2(  6.00f, 0.04055144f),
-		math::float2(  7.00f, 0.03468581f),
-		math::float2(  8.00f, 0.02896398f),
-		math::float2(  9.00f, 0.02361161f),
-		math::float2( 10.00f, 0.01879116f),
-		math::float2( 11.00f, 0.01459965f),
-		math::float2( 12.00f, 0.01107369f),
-		math::float2( 13.00f, 0.00819980f),
-		math::float2( 14.00f, 0.00592754f),
-		math::float2( 15.00f, 0.00418319f),
-		math::float2( 16.00f, 0.00288204f)
+		math::float2(-9.00f, 0.02361161f),
+		math::float2(-8.00f, 0.02896398f),
+		math::float2(-7.00f, 0.03468581f),
+		math::float2(-6.00f, 0.04055144f),
+		math::float2(-5.00f, 0.04628301f),
+		math::float2(-4.00f, 0.05157007f),
+		math::float2(-3.00f, 0.05609637f),
+		math::float2(-2.00f, 0.05957069f),
+		math::float2(-1.00f, 0.06175773f),
+		math::float2(0.00f, 0.06250444f),
+		math::float2(1.00f, 0.06175773f),
+		math::float2(2.00f, 0.05957069f),
+		math::float2(3.00f, 0.05609637f),
+		math::float2(4.00f, 0.05157007f),
+		math::float2(5.00f, 0.04628301f),
+		math::float2(6.00f, 0.04055144f),
+		math::float2(7.00f, 0.03468581f),
+		math::float2(8.00f, 0.02896398f),
+		math::float2(9.00f, 0.02361161f),
+		math::float2(10.00f, 0.01879116f),
+		math::float2(11.00f, 0.01459965f),
+		math::float2(12.00f, 0.01107369f),
+		math::float2(13.00f, 0.00819980f),
+		math::float2(14.00f, 0.00592754f),
+		math::float2(15.00f, 0.00418319f),
+		math::float2(16.00f, 0.00288204f)
 	};
 }
 
 template<typename T>
-void swap(T& a,T& b)
+void swap(T& a, T& b)
 {
 	T c = b;
 	b = a;
 	a = c;
 }
 
-__global__ void luminance_kernel(float * dest, const float * input, unsigned int width, unsigned int height) {
+// kernel that computes average luminance of a pixel
+__global__ void luminance_kernel(float* dest, const float* input, unsigned int width, unsigned int height)
+{
+	// each thread needs to know on which pixels to work -> get absolute coordinates of the thread in the grid
 	unsigned int x = blockIdx.x * blockDim.x + threadIdx.x;
 	unsigned int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+	// input is stored as array (row-wise). Load the pixel values
+	// offset of first pixel  = y coordinates * width of block (b/c full rows already read) + x coordinates
+
 	if (x < width && y < height) {
-		const float * input_pixel = input + (width * y + x) * 3;
-		float lum = (0.2126f * input_pixel[0] + 0.7152f * input_pixel[1] + 0.0722f * input_pixel[2]) ;
-		dest[width * y + x] = lum;
+		const float* input_pixel = input + 3 * (width*y + x); // *3 b/c three colors (=bytes) per pixel.
+
+		float lum = 0.21f * input_pixel[0] + 0.72f * input_pixel[1] + 0.07f * input_pixel[2];  //compensate for human vision
+
+		dest[width *y + x] = lum; //store the results. not * 3 b/c only luminance, no colors
 	}
 }
 
-__host__ void luminance(float * dest, const float * input, unsigned int width, unsigned int height) {
-	const dim3 block_size = { 32, 32};
-	const dim3 num_blocks = { divup(width, block_size.x), divup(height, block_size.y)};
-	luminance_kernel <<< num_blocks, block_size >>> (dest, input, width, height);
+// get the required number of blocks to cover the whole image, and run the kernel on all blocks
+void luminance(float* dest, const float* input, unsigned int width, unsigned int height) {
+	const dim3 block_size = { 32, 32 };
+	// calculate number of blocks required to process the whole image -> round up to the next multiple of 32 (full block)
+	const dim3 num_blocks = {
+		divup(width, block_size.x),
+		divup(height, block_size.y)
+	};
+
+	// launch the kernel that we wrote above for all the blocks
+	luminance_kernel << <num_blocks, block_size >> >(dest, input, width, height);
+
+	// this 'downsampling' step takes about 1.3ms on a GT 730m
+	// now we want to run this in a hierarchical way: reduce the image in each step
+	// -> launch blocks on the images, reducing its size each step until we end up with a 1x1 color
+	// -> we need to allocate memory for a buffer in the HDRPipeline object declaration
 }
 
-#define F 2
-__global__ void downsample_kernel(float * output, float * luminance, unsigned int width, unsigned int height, unsigned int pitch) {
+__global__ void downsample_kernel(float* dest, float* input, unsigned int width, unsigned int height) {
+	// each thread needs to know on which pixels to work -> get absolute coordinates of the thread in the grid
 	unsigned int x = blockIdx.x * blockDim.x + threadIdx.x;
 	unsigned int y = blockIdx.y * blockDim.y + threadIdx.y;
 
-	
-	
+	int F = 2; //width of downsampling square (F^2 = number of pixels to be pooled together)
+
+	if ((x >= width / F) || (y >= height / F))
+		return;
+
 	float sum = 0.0f;
-	if (x >= width / F || y >= height / F) return;
-	for (int j = 0; j < F; j++) {
+
+	// 2D version
+	for (int j = 0; j<F; j++) {
 		for (int i = 0; i < F; i++) {
-			sum += luminance[(y*F + j) * pitch + (x * F + i)];
+			// current pixel : (x+i, y+j)
+			sum += input[(y*F + j) * width + x*F + i];
 		}
 	}
-	//output[0] = sum / (F*F);
-	output[y * pitch / F + x] = sum / (F * F);
+	dest[y* width / F + x] = sum / (F * F);
+
+	printf(" KERNEL width %d | height %d \n", width, height);
+
+
+	// 1D version : F is amount of pixels we pool. Only works for F=2, not F=4 as would make more sense. Weird.
+	//		for (int i = 0; i< F; i++){
+	//			sum += input[F * (y * width + x) + i]; //jump with step pool_size
+	//		}
+	//		dest[y * width /F + x] = sum / F; //store the results. not * 3 b/c only luminance, no colors
 }
 
-__host__ void downsample(float * output, float * luminance, unsigned int width, unsigned int height) {
-	printf("output %p luminance %p\n", output, luminance);
+// get the required number of blocks to cover the whole image, and run the kernel on all blocks
+void downsample(float* dest, float* luminance, unsigned int width, unsigned int height) {
 	const dim3 block_size = { 32, 32 };
-	const dim3 num_blocks = { divup(width, block_size.x), divup(height, block_size.y) };
-	unsigned int pitch = width;
-	bool ping = false;
-	while (width != 1 || height != 1) {
-		//downsample_kernel << <num_blocks, block_size >> > ((ping) ? output : luminance, (ping) ? luminance : output, width, height, pitch);
+	// calculate number of blocks required to process the whole image -> round up to the next multiple of 32 (full block)
+	const dim3 num_blocks = {
+		divup(width, block_size.x),
+		divup(height, block_size.y)
+	};
+
+	int ping = 0; //result in dest buffer
+				  // first iteration
+	downsample_kernel << <num_blocks, block_size >> >(dest, luminance, width, height);
+	ping = 0;
+
+	while (width != 1 && height != 1) {
+		// result will be in the dest buffer
+		width = width / 2;
+		height = height / 2;
+
+		printf(" width %d | height %d \n", width, height);
 		if (ping) {
-			downsample_kernel << <num_blocks, block_size >> > (luminance, output, width, height, pitch);
-		} else {
-			downsample_kernel << <num_blocks, block_size >> > (output, luminance, width, height, pitch/2);
+			downsample_kernel << <num_blocks, block_size >> >(dest, luminance, width, height);
+			ping = 0;
 		}
-		width = (width > 1) ? width / 2 : 1;
-		height = (height > 1) ? height / 2 : 1;
-		ping = !ping;	
-		cudaDeviceSynchronize();
-	} 
-	printf("Done\n");
+		else {
+			// now ping-pong; result will be in the luminance buffer
+			downsample_kernel << <num_blocks, block_size >> >(luminance, dest, width, height);
+			ping = 1;
+		}
+	}
+	// make sure the result is stored in dest
+	if (ping) {
+		cudaMemcpy(luminance, dest, 1, cudaMemcpyDeviceToDevice);
+	}
 }
+
+
 // TODO: implement gaussian blur for light bloom
 
 __global__ void tonemap_kernel(uchar4* tonemapped, uchar4* brightpass, const float* src, unsigned int width, unsigned int height, float exposure, float brightpass_thdesthold)
@@ -130,15 +181,15 @@ __global__ void tonemap_kernel(uchar4* tonemapped, uchar4* brightpass, const flo
 		// write out tonemapped color
 		uchar4 out = { toSRGB8(c_t.x), toSRGB8(c_t.y), toSRGB8(c_t.z), 0xFFU };
 		tonemapped[y * width + x] = out;
-		brightpass[y * width + x] = luminance(c_t) > brightpass_thdesthold ? out : uchar4 { 0U, 0U, 0U, 0xFFU };
+		brightpass[y * width + x] = luminance(c_t) > brightpass_thdesthold ? out : uchar4{ 0U, 0U, 0U, 0xFFU };
 	}
 }
 
 void tonemap(uchar4* tonemapped, uchar4* brightpass, const float* src, unsigned int width, unsigned int height, float exposure, float brightpass_thdesthold)
 {
-	const auto block_size = dim3 { 32U, 32U };
+	const auto block_size = dim3{ 32U, 32U };
 
 	auto num_blocks = dim3{ divup(width, block_size.x), divup(height, block_size.y) };
 
-	tonemap_kernel<<<num_blocks, block_size>>>(tonemapped, brightpass, src, width, height, exposure, brightpass_thdesthold);
+	tonemap_kernel << <num_blocks, block_size >> >(tonemapped, brightpass, src, width, height, exposure, brightpass_thdesthold);
 }
