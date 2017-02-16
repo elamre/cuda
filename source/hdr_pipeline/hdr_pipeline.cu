6f9c659b7407f94d017dde99b8cb678d48f7094b
elamre <e.vanrijnswou@innoseis.com>
2017-02-16 17:47:06 +0100

    pitc/2

--- source/hdr_pipeline/hdr_pipeline.cu
+++ source/hdr_pipeline/hdr_pipeline.cu
@@ -102,7 +102,7 @@ __host__ void downsample(float * output, float * luminance, unsigned int width, 
 		if (ping) {
 			downsample_kernel << <num_blocks, block_size >> > (luminance, output, width, height, pitch);
 		} else {
-			downsample_kernel << <num_blocks, block_size >> > (output, luminance, width, height, pitch);
+			downsample_kernel << <num_blocks, block_size >> > (output, luminance, width, height, pitch/2);
 		}
 		width = (width > 1) ? width / 2 : 1;
 		height = (height > 1) ? height / 2 : 1;
