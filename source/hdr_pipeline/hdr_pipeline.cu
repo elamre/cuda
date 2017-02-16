ad91f16fdbe4e53dd585fc7a652563ddfc9f248a
elamre <e.vanrijnswou@innoseis.com>
2017-02-16 12:45:24 +0100

    Debug statements

--- source/hdr_pipeline/hdr_pipeline.cu
+++ source/hdr_pipeline/hdr_pipeline.cu
@@ -92,12 +92,14 @@ __host__ void downsample(float * output, float * luminance, unsigned int width, 
 	const dim3 block_size = { 32, 32 };
 	const dim3 num_blocks = { divup(width, block_size.x), divup(height, block_size.y) };
 	bool ping = false;
+	printf("Start to test\n");
 	do {
 		downsample_kernel <<<num_blocks, block_size>>> ((ping)? output : luminance, (ping) ? luminance : output, width, height);
-		width = (width > 2) ? width / 2 : 2;
-		height = (height > 2) ? height / 2 : 2;
+		width = (width > 1) ? width / 2 : 1;
+		height = (height > 1) ? height / 2 : 1;
 		ping = !ping;
-	} while (width != 2 || height != 2);
+		printf("width %d height %d\n", width, height);
+	} while (width != 1 && height != 1);
 }
 // TODO: implement gaussian blur for light bloom
 
