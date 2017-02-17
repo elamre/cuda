78db226eed9f79872aa7818b40a891d9bebd0a3e
elamre <e.vanrijnswou@innoseis.com>
2017-02-17 09:30:51 +0100

    new version

--- source/hdr_pipeline/HDRPipeline.cpp
+++ source/hdr_pipeline/HDRPipeline.cpp
@@ -15,85 +15,115 @@ namespace
 		throw_error(::cudaMalloc(&ptr, size * sizeof(T)));
 		return cuda_unique_ptr<T> { static_cast<T*>(ptr) };
 	}
+
+	template <typename T>
+	auto cudaMallocZeroed(std::size_t size)
+	{
+		auto memory = cudaMalloc<T>(size);
+		throw_error(cudaMemset(memory.get(), 0, size * sizeof(T)));
+		return memory;
+	}
 }
 
-void * downsample_buffer;
-
 HDRPipeline::HDRPipeline(unsigned int width, unsigned int height)
-	: d_input_image(cudaMalloc<float>(width * height * 3)),
-	  d_luminance_image(cudaMalloc<float>(width * height)),
-	  d_buffer_image(cudaMalloc<float>(width * height)),
-	  d_brightpass_image(cudaMalloc<uchar4>(width * height)),
-	  d_output_image(cudaMalloc<uchar4>(width * height)),
-	  width(width),
-	  height(height)
+	: width(width),
+	  height(height),
+	  d_input_image(cudaMalloc<float>(width * height * 3)),
+	  d_luminance_image(cudaMallocZeroed<float>(width * height)),
+	  d_downsample_buffer(cudaMallocZeroed<float>(width * height)),
+	  d_tonemapped_image(cudaMallocZeroed<float>(width * height * 3)),
+	  d_brightpass_image(cudaMallocZeroed<float>(width * height * 3)),
+	  d_blurred_image(cudaMallocZeroed<float>(width * height * 3)),
+	  d_output_image(cudaMallocZeroed<float>(width * height * 3))
 {
-	throw_error(cudaMalloc(&downsample_buffer, width * height * sizeof(float) / 4));
 }
 
 void HDRPipeline::consume(const float* input_image)
 {
-	// copy input data to GPU
+	// upload input data to GPU
 	throw_error(cudaMemcpy(d_input_image.get(), input_image, width * height * 3 * 4U, cudaMemcpyHostToDevice));
+}
+
+void HDRPipeline::computeLuminance()
+{
+	void luminance(float* dest, const float* src, unsigned int width, unsigned int height);
+
+	luminance(d_luminance_image.get(), d_input_image.get(), width, height);
 }
 
 float HDRPipeline::downsample()
 {
-	//luminance
-	// TODO: implement downsampling and return average luminance
-	void luminance(float * dest, const float * input, unsigned int width, unsigned int height);
-	void downsample(float * output, float * luminance, unsigned int width, unsigned int height);
-	
-	luminance(d_luminance_image.get(), d_input_image.get(), width, height);
-	printf("output %p luminance %p\n", (float *) downsample_buffer, d_luminance_image.get());
-	downsample((float*)downsample_buffer, d_luminance_image.get(), width, height);
-	printf("output %p luminance %p\n", (float *)downsample_buffer, d_luminance_image.get());
-	return 1.0;
+	float downsample(float* buffer, float* src, unsigned int width, unsigned int height);
+
+	return downsample(d_downsample_buffer.get(), d_luminance_image.get(), width, height);
 }
 
 void HDRPipeline::tonemap(float exposure, float brightpass_threshold)
 {
-	void tonemap(uchar4* tonemapped, uchar4* brightpass, const float* src, unsigned int width, unsigned int height, float exposure, float brightpass_threshold);
+	void tonemap(float* tonemapped, float* brightpass, const float* src, unsigned int width, unsigned int height, float exposure, float brightpass_threshold);
 
-	tonemap(d_output_image.get(), d_brightpass_image.get(), d_input_image.get(), width, height, exposure, brightpass_threshold);
+	tonemap(d_tonemapped_image.get(), d_brightpass_image.get(), d_input_image.get(), width, height, exposure, brightpass_threshold);
+}
 
-	// TODO: implement gaussian blur of brightpass
+void HDRPipeline::blur()
+{
+	void gaussian_blur(float* dest, const float* src, unsigned int width, unsigned int height);
+
+	gaussian_blur(d_blurred_image.get(), d_brightpass_image.get(), width, height);
 }
 
 void HDRPipeline::compose()
 {
-	// TODO: compose tonemapped image with blurred brightpass
+	void compose(float* output, const float* tonemapped, const float* blurred, unsigned int width, unsigned int height);
+
+	compose(d_output_image.get(), d_tonemapped_image.get(), d_brightpass_image.get(), width, height);
 }
 
 
 image<float> HDRPipeline::readLuminance()
 {
-	image<float> output(width, height);
-	// copy back output data from GPU
-	throw_error(cudaMemcpy(data(output), d_luminance_image.get(), width * height * 4U, cudaMemcpyDeviceToHost));
-	return output;
+	image<float> luminance(width, height);
+	// download output data from GPU
+	throw_error(cudaMemcpy(data(luminance), d_luminance_image.get(), width * height * 4U, cudaMemcpyDeviceToHost));
+	return luminance;
 }
 
 image<float> HDRPipeline::readDownsample()
 {
-	image<float> output(width/2, height/2);
-	// copy back output data from GPU
-	throw_error(cudaMemcpy(data(output), downsample_buffer, width/2 * height/2 * 4U, cudaMemcpyDeviceToHost));
-	return output;
+	image<float> downsample(width, height);
+	// download output data from GPU
+	throw_error(cudaMemcpy(data(downsample), d_downsample_buffer.get(), width * height * 4U, cudaMemcpyDeviceToHost));
+	return downsample;
 }
 
-image<std::uint32_t> HDRPipeline::readBrightpass()
+image<RGB32F> HDRPipeline::readTonemapped()
 {
-	image<std::uint32_t> output(width, height);
-	// copy back output data from GPU
-	throw_error(cudaMemcpy(data(output), d_brightpass_image.get(), width * height * 4U, cudaMemcpyDeviceToHost));
-	return output;
+	image<RGB32F> tonemapped(width, height);
+	// download output data from GPU
+	throw_error(cudaMemcpy(data(tonemapped), d_tonemapped_image.get(), width * height * 3 * 4U, cudaMemcpyDeviceToHost));
+	return tonemapped;
 }
 
-image<std::uint32_t> HDRPipeline::readOutput()
+image<RGB32F> HDRPipeline::readBrightpass()
 {
-	image<std::uint32_t> output(width, height);
-	// copy back output data from GPU
-	throw_error(cudaMemcpy(data(output), d_output_image.get(), width * height * 4U, cudaMemcpyDeviceToHost));
+	image<RGB32F> brightpass(width, height);
+	// download output data from GPU
+	throw_error(cudaMemcpy(data(brightpass), d_brightpass_image.get(), width * height * 3 * 4U, cudaMemcpyDeviceToHost));
+	return brightpass;
+}
+
+image<RGB32F> HDRPipeline::readBlurred()
+{
+	image<RGB32F> blurred(width, height);
+	// download output data from GPU
+	throw_error(cudaMemcpy(data(blurred), d_blurred_image.get(), width * height * 3 * 4U, cudaMemcpyDeviceToHost));
+	return blurred;
+}
+
+image<RGB32F> HDRPipeline::readOutput()
+{
+	image<RGB32F> output(width, height);
+	// download output data from GPU
+	throw_error(cudaMemcpy(data(output), d_output_image.get(), width * height * 3 * 4U, cudaMemcpyDeviceToHost));
 	return output;
 }
--- source/hdr_pipeline/hdr_pipeline.cu
+++ source/hdr_pipeline/hdr_pipeline.cu
@@ -13,104 +13,62 @@ namespace
 		return (a + b - 1) / b;
 	}
 
-	const math::float2 gauss_kernel[] = {
-		math::float2(-16.00f, 0.00288204f),
-		math::float2(-15.00f, 0.00418319f),
-		math::float2(-14.00f, 0.00592754f),
-		math::float2(-13.00f, 0.00819980f),
-		math::float2(-12.00f, 0.01107369f),
-		math::float2(-11.00f, 0.01459965f),
-		math::float2(-10.00f, 0.01879116f),
-		math::float2( -9.00f, 0.02361161f),
-		math::float2( -8.00f, 0.02896398f),
-		math::float2( -7.00f, 0.03468581f),
-		math::float2( -6.00f, 0.04055144f),
-		math::float2( -5.00f, 0.04628301f),
-		math::float2( -4.00f, 0.05157007f),
-		math::float2( -3.00f, 0.05609637f),
-		math::float2( -2.00f, 0.05957069f),
-		math::float2( -1.00f, 0.06175773f),
-		math::float2(  0.00f, 0.06250444f),
-		math::float2(  1.00f, 0.06175773f),
-		math::float2(  2.00f, 0.05957069f),
-		math::float2(  3.00f, 0.05609637f),
-		math::float2(  4.00f, 0.05157007f),
-		math::float2(  5.00f, 0.04628301f),
-		math::float2(  6.00f, 0.04055144f),
-		math::float2(  7.00f, 0.03468581f),
-		math::float2(  8.00f, 0.02896398f),
-		math::float2(  9.00f, 0.02361161f),
-		math::float2( 10.00f, 0.01879116f),
-		math::float2( 11.00f, 0.01459965f),
-		math::float2( 12.00f, 0.01107369f),
-		math::float2( 13.00f, 0.00819980f),
-		math::float2( 14.00f, 0.00592754f),
-		math::float2( 15.00f, 0.00418319f),
-		math::float2( 16.00f, 0.00288204f)
-	};
+	template<typename T>
+	void swap(T& a, T& b)
+	{
+		T c = b;
+		b = a;
+		a = c;
+	}
 }
 
-template<typename T>
-void swap(T& a,T& b)
+
+
+void luminance(float* dest, const float* src, unsigned int width, unsigned int height)
 {
-	T c = b;
-	b = a;
-	a = c;
+	// TODO: compute luminance of src image
 }
 
-__global__ void luminance_kernel(float * dest, const float * input, unsigned int width, unsigned int height) {
-	unsigned int x = blockIdx.x * blockDim.x + threadIdx.x;
-	unsigned int y = blockIdx.y * blockDim.y + threadIdx.y;
-	if (x < width && y < height) {
-		const float * input_pixel = input + (width * y + x) * 3;
-		float lum = (0.2126f * input_pixel[0] + 0.7152f * input_pixel[1] + 0.0722f * input_pixel[2]) ;
-		dest[width * y + x] = lum;
-	}
+
+
+float downsample(float* buffer, float* src, unsigned int width, unsigned int height)
+{
+	// TODO: downsample and return average luminance
+
+	return 1.0f;
 }
 
-__host__ void luminance(float * dest, const float * input, unsigned int width, unsigned int height) {
-	const dim3 block_size = { 32, 32};
-	const dim3 num_blocks = { divup(width, block_size.x), divup(height, block_size.y)};
-	luminance_kernel <<< num_blocks, block_size >>> (dest, input, width, height);
+
+
+__global__ void blur_kernel(float* dest, const float* src, unsigned int width, unsigned int height)
+{
+	constexpr float weights[] = {
+		//  -16           -15         -14          -13          -12          -11          -10           -9           -8           -7           -6           -5           -4           -3           -2           -1
+		0.00288204f, 0.00418319f, 0.00592754f, 0.00819980f, 0.01107369f, 0.01459965f, 0.01879116f, 0.02361161f, 0.02896398f, 0.03468581f, 0.04055144f, 0.04628301f, 0.05157007f, 0.05609637f, 0.05957069f, 0.06175773f,
+		//    0
+		0.06250444f,
+		//    1             2           3            4            5            6            7            8            9           10           11           12           13           14           15           16
+		0.06175773f, 0.05957069f, 0.05609637f, 0.05157007f, 0.04628301f, 0.04055144f, 0.03468581f, 0.02896398f, 0.02361161f, 0.01879116f, 0.01459965f, 0.01107369f, 0.00819980f, 0.00592754f, 0.00418319f, 0.00288204f
+	};
+
+
 }
 
-#define F 2
-__global__ void downsample_kernel(float * output, float * luminance, unsigned int width, unsigned int height, unsigned int outputPitch, unsigned int  inputPitch) {
-	unsigned int x = blockIdx.x * blockDim.x + threadIdx.x;
-	unsigned int y = blockIdx.y * blockDim.y + threadIdx.y;
-	float sum = 0.0f;
-	if (x >= width / F || y >= height / F) return;
-	for (int j = 0; j < F; j++) {
-		for (int i = 0; i < F; i++) {
-			sum += luminance[(y*F + j) * inputPitch + (x * F + i)];
-		}
-	}
-	output[y * outputPitch/F + x] = sum / (F * F);
+void gaussian_blur(float* dest, const float* src, unsigned int width, unsigned int height)
+{
+	// TODO: gaussian blur of brightpass
 }
 
-__host__ void downsample(float * output, float * luminance, unsigned int width, unsigned int height) {
-	printf("output %p luminance %p\n", output, luminance);
-	const dim3 block_size = { 32, 32 };
-	const dim3 num_blocks = { divup(width, block_size.x), divup(height, block_size.y) };
-	unsigned int pitch = width;
-	bool ping = false;
-	while (width != 1 || height != 1) {
-		//downsample_kernel << <num_blocks, block_size >> > ((ping) ? output : luminance, (ping) ? luminance : output, width, height, pitch);
-		if (ping) {
-			downsample_kernel << <num_blocks, block_size >> > (luminance, output, width, height, pitch, pitch/2);
-		} else {
-			downsample_kernel << <num_blocks, block_size >> > (output, luminance, width, height, pitch/2, pitch);
-		}
-		width = (width > 1) ? width / 2 : 1;
-		height = (height > 1) ? height / 2 : 1;
-		ping = !ping;	
-		cudaDeviceSynchronize();
-	} 
-	printf("Done\n");
-}
-// TODO: implement gaussian blur for light bloom
 
-__global__ void tonemap_kernel(uchar4* tonemapped, uchar4* brightpass, const float* src, unsigned int width, unsigned int height, float exposure, float brightpass_thdesthold)
+
+void compose(float* output, const float* tonemapped, const float* blurred, unsigned int width, unsigned int height)
+{
+	// TODO: add blurred brightpass to tonemapped image
+}
+
+
+
+__global__ void tonemap_kernel(float* tonemapped, float* brightpass, const float* src, unsigned int width, unsigned int height, float exposure, float brightpass_threshold)
 {
 	unsigned int x = blockIdx.x * blockDim.x + threadIdx.x;
 	unsigned int y = blockIdx.y * blockDim.y + threadIdx.y;
@@ -124,17 +82,23 @@ __global__ void tonemap_kernel(uchar4* tonemapped, uchar4* brightpass, const flo
 		math::float3 c_t = tonemap(c, exposure);
 
 		// write out tonemapped color
-		uchar4 out = { toSRGB8(c_t.x), toSRGB8(c_t.y), toSRGB8(c_t.z), 0xFFU };
-		tonemapped[y * width + x] = out;
-		brightpass[y * width + x] = luminance(c_t) > brightpass_thdesthold ? out : uchar4 { 0U, 0U, 0U, 0xFFU };
+		tonemapped[3 * (y * width + x) + 0] = c_t.x;
+		tonemapped[3 * (y * width + x) + 1] = c_t.y;
+		tonemapped[3 * (y * width + x) + 2] = c_t.z;
+
+		// write out brightpass color
+		math::float3 c_b = luminance(c_t) > brightpass_threshold ? c_t : math::float3 {0.0f, 0.0f, 0.0f};
+		brightpass[3 * (y * width + x) + 0] = c_b.x;
+		brightpass[3 * (y * width + x) + 1] = c_b.y;
+		brightpass[3 * (y * width + x) + 2] = c_b.z;
 	}
 }
 
-void tonemap(uchar4* tonemapped, uchar4* brightpass, const float* src, unsigned int width, unsigned int height, float exposure, float brightpass_thdesthold)
+void tonemap(float* tonemapped, float* brightpass, const float* src, unsigned int width, unsigned int height, float exposure, float brightpass_threshold)
 {
 	const auto block_size = dim3 { 32U, 32U };
 
 	auto num_blocks = dim3{ divup(width, block_size.x), divup(height, block_size.y) };
 
-	tonemap_kernel<<<num_blocks, block_size>>>(tonemapped, brightpass, src, width, height, exposure, brightpass_thdesthold);
+	tonemap_kernel<<<num_blocks, block_size>>>(tonemapped, brightpass, src, width, height, exposure, brightpass_threshold);
 }
