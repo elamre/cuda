d11dad7a1bf42455313d53a39757398f8301f5bf
elamre <e.vanrijnswou@innoseis.com>
2017-02-17 14:54:17 +0100

    gaussian

--- source/hdr_pipeline/hdr_pipeline.cu
+++ source/hdr_pipeline/hdr_pipeline.cu
@@ -60,27 +60,36 @@ void luminance(float* dest, const float* input, unsigned int width, unsigned int
 }
 
 __global__ void downsample_kernel(float* dest, float* input, unsigned int width, unsigned int height, unsigned int outputPitch, unsigned int inputPitch) {
-	// each thread needs to know on which pixels to work -> get absolute coordinates of the thread in the grid
+	// each thread needs to know on which pixels to work -> get absolute coordinates for/of the thread in the grid
 	unsigned int x = blockIdx.x * blockDim.x + threadIdx.x;
 	unsigned int y = blockIdx.y * blockDim.y + threadIdx.y;
 
 	int F = 2; //width of downsampling square (F^2 = number of pixels to be pooled together)
 
 			   //printf(" KERNEL width %d | height %d \n", width, height);
-	if ((x >= width / F) || (y >= height / F))
+	if ((x*F > width - 1) || (y*F > height - 1))
 		return;
 
 	float sum = 0.0f;
 
-	// 2D version: add pixels in 2x2 block, calculate the average. Jump with F so different threads don't operate on the same pixels.
+	int nb_counted = F * F;
+	// number of pixels to be counted: We start at pixel (x*F, y*F), continue till coordinate(width-1, height-1)
+	int xDim = min(F, width - x*F);
+	int yDim = min(F, height - y*F);
+	//printf("xDim: %d, yDim: %d \n", xDim, yDim);
+	nb_counted = xDim * yDim;
+	//printf(" counted %d | width %d | height %d | \t x %d | y %d \n", nb_counted, width, height, x*F, y*F);
 
+	// 2D version: add pixels in 2x2 block, calculate the average. Jump with F so different threads don't operate on the same pixels.
 	for (int j = 0; j<F; j++) {
 		for (int i = 0; i < F; i++) {
-			// current pixel : (x+i, y+j)
-			sum += input[(y*F + j) * inputPitch + x*F + i];
+			// current pixel : (x*F+i, y*F+j)
+			if ((y*F + j) < height && (x*F + i) < width) { //only sum pixels inside the image, don't count overlapping at the right or bottom sides
+				sum += input[(y*F + j) * inputPitch + x*F + i];
+			}
 		}
 	}
-	dest[y* outputPitch + x] = sum / (F * F);
+	dest[y* outputPitch + x] = sum / nb_counted;
 }
 
 // get the required number of blocks to cover the whole image, and run the kernel on all blocks
@@ -135,11 +144,13 @@ float downsample(float* dest, float* luminance, unsigned int width, unsigned int
 	return average;
 }
 
+// first do it on the x direction
+// then do it on the y direction (-> texture memory to make this fast)
 
-__global__ void blur_kernel(float* dest, const float* src, unsigned int width, unsigned int height)
+__global__ void blur_kernel_x(float* dest, const float* src, unsigned int width, unsigned int height, unsigned int inputPitch, unsigned int outputPitch)
 {
-	constexpr float weights[] = {
-		//  -16           -15         -14          -13          -12          -11          -10           -9           -8           -7           -6           -5           -4           -3           -2           -1
+	constexpr float weights[] = {  //weights in one dimension -> 33x33 filter
+								   //  -16           -15         -14          -13          -12          -11          -10           -9           -8           -7           -6           -5           -4           -3           -2           -1
 		0.00288204f, 0.00418319f, 0.00592754f, 0.00819980f, 0.01107369f, 0.01459965f, 0.01879116f, 0.02361161f, 0.02896398f, 0.03468581f, 0.04055144f, 0.04628301f, 0.05157007f, 0.05609637f, 0.05957069f, 0.06175773f,
 		//    0
 		0.06250444f,
@@ -147,12 +158,33 @@ __global__ void blur_kernel(float* dest, const float* src, unsigned int width, u
 		0.06175773f, 0.05957069f, 0.05609637f, 0.05157007f, 0.04628301f, 0.04055144f, 0.03468581f, 0.02896398f, 0.02361161f, 0.01879116f, 0.01459965f, 0.01107369f, 0.00819980f, 0.00592754f, 0.00418319f, 0.00288204f
 	};
 
+	// 1 thread per output pixel
+	unsigned int x = blockIdx.x * blockDim.x + threadIdx.x;
+	unsigned int y = blockIdx.y * blockDim.y + threadIdx.y;
 
+	float sum = 0.0f;
+
+	for (int i = 0, l = sizeof(weights) / sizeof(float); i < l; i++) {
+		if ((i + x - l/2 >= 0) && (i + x + l/2 < width)) {
+			sum += src[y*inputPitch + x + i] * weights[i];
+		}
+	} 
+	printf("sum: %d \n", sum);
+	dest[y* outputPitch + x] = sum;
 }
 
 void gaussian_blur(float* dest, const float* src, unsigned int width, unsigned int height)
 {
-	// TODO: gaussian blur of brightpass
+	const dim3 block_size = { 32, 32 };
+	// calculate number of blocks required to process the whole image -> round up to the next multiple of 32 (full block)
+	const dim3 num_blocks = {
+		divup(width, block_size.x),
+		divup(height, block_size.y)
+	};
+
+	int inputPitch = width;
+	int outputPitch = width;
+	blur_kernel_x << <num_blocks, block_size >> >(dest, src, width, height, inputPitch, outputPitch);
 }
 
 
